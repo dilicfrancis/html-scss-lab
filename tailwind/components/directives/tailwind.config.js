53fa59683c9154779ee791e9da8fd93a1a8b31e0
Dili C. Francis (MacBook) <email@dilicfrancis.com>
2024-02-12 12:46:09 -0800

    first commit

---
+++ tailwind/components/directives/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        96: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
